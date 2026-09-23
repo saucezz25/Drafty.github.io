e42c470b7727784f3e43e62c56713cbfc135f723
saucezz25 <engelwurzthe@gmail.com>
2026-09-23 18:19:49 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'drafty-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
